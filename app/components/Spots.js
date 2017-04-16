c707e4cea43b580726b5fb2f6fe33fa5ae5cca9d
Jeremy Monson <monson.jeremy@gmail.com>
2017-04-15 19:40:12 -0700

    Added some state functionality, loading animation, and some stateless components

--- app/components/Spots.js
+++ app/components/Spots.js
@@ -20,7 +20,7 @@ function SelectSpot (props) {
                             style = {spot.name === props.selectedSpot.name ? {color: '#d0021b'}: null}
                             onClick={props.onSelect.bind(null, spot)}
                             key={spot.name}>
-                            {spot.name} + {spot.id}
+                            {spot.name}
                         </li>
                     )
                 })}
@@ -34,31 +34,88 @@ SelectSpot.propTypes = {
     onSelect: PropTypes.func.isRequired,
 }
 
+ForecastBox.propTypes = {
+    forecast: PropTypes.object.isRequired,
+}
+
+DayContainer.propTypes = {
+    date: PropTypes.array.isRequired,
+}
+
+function ForecastBox(props){
+    return (
+        <div>
+        {props.forecast.dateStamp.map((date, index) => {
+            return (
+                <div key={'date' + index} className='day-box'>
+                    <DayContainer date={date} />
+                </div>
+            )
+        })}
+        </div>
+    )
+}
+
+function DayContainer(props){
+    return (
+        <div className='forecast-by-day'>
+        {props.date.map( (date, index) => {
+        return (
+            date
+            /*<ul className='forecast-list'>
+                {props.forecast.dateStamp.map((dateStamps, indexone) => {
+                    {dateStamps.map((date, indextwo) => {
+                        return (
+                            <li key={date} className='date'>
+                                {console.log('date: ' + JSON.stringify(date))}
+                                {console.log('surf: ' + props.forecast.surf_max[indexone][indextwo])}
+                                <div>{date}</div>
+                                <div>{props.forecast.surf_max[indexone][indextwo]}</div>
+                            </li>
+                        )
+                    })}
+                })}
+                <li className='surf-forecast'>
+                </li>
+            </ul>
+            */
+        )
+    })}
+    </div>
+    )
+}
+
 class Spots extends React.Component {
 
     constructor (props) {
         super(props)
         this.state = {
-            selectedSpot: {name: 'Santa Cruz', id: '2958'}
+            selectedSpot: {name: 'Santa Cruz', id: '2958'},
+            forecast: null
         }
 
         this.updateSpot = this.updateSpot.bind(this)
     }
 
     componentDidMount () {
-        api.fetchSpot(this.state.selectedSpot.id)
-        .then(function (spotInfo) {
-            console.log(spotInfo)
-        })
+        this.updateSpot(this.state.selectedSpot)
     }
     
     updateSpot(spot) {
-        console.log(spot)
         this.setState(function () {
             return {
                 selectedSpot: spot,
             }
         })
+
+        api.fetchSpot(spot.id)
+        .then(function (forecast) {
+            this.setState(function () {
+                return {
+                    forecast: forecast,
+                }
+            })
+        }.bind(this))
     }
 
     render() {
@@ -68,6 +125,14 @@ class Spots extends React.Component {
                     selectedSpot={this.state.selectedSpot}
                     onSelect={this.updateSpot}
                     />
+                <div>
+                    {!this.state.forecast ? 
+                        <div className="loading">
+                            <div className="squareXS"></div>
+                            <div className="squareXL"></div>
+                        </div>
+                    : <ForecastBox forecast={this.state.forecast} /> }
+                </div>
             </div>
         )
     }
