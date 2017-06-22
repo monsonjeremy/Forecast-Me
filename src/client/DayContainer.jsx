e8b523a1d59c255963c9e7a30b1a344fce2ae907
Jeremy Monson <monson.jeremy@gmail.com>
2017-06-22 09:51:26 -0700

    Cleaned up some components to make the code more readable

--- src/client/DayContainer.jsx
+++ src/client/DayContainer.jsx
@@ -3,24 +3,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import moment from 'moment'
-import StackedBarChart from './components/stacked_bar_chart/StackedBarChart'
+import GraphContainer from './components/GraphContainer'
 import '../assets/stylesheets/daycontainer.scss'
 
 type Props = {
-  date: Array<string>,
-  forecast: Object,
+  date: Array<String>,
+  forecast: Array<Object>,
   maxSurf: number,
-  dataKeys: Array<string>,
+  dataKeys: Array<String>,
 }
 
 const DayContainer = ({ date, forecast, maxSurf, dataKeys }: Props) => {
-  const surfChartWidth = '85%'
-
+  // Format the date in the Title section of the container ('Wed June 21st')
   const dateTitle = moment(date[0], 'MMMM DD, YYYY HH:mm:ss').format('ddd MMMM Do')
-  // const tideData = filterTideData(dateTitle, forecast.Tide.dataPoints)
-  const tideChartWidth = '85%'
 
+  // This could maybe be moved to a different component
+  // Keeping it here until other graphs are finished in case graphs within the same day container
+  // need to know each others information
   const params = {
+    data: forecast,
     width: 300,
     height: 500,
     axisMargin: 83,
@@ -30,8 +31,6 @@ const DayContainer = ({ date, forecast, maxSurf, dataKeys }: Props) => {
     keys: dataKeys,
   }
 
-  const fullWidth = 500
-
   return (
     <div className="days row">
       <div className="day-date-title col-xs-12 col-md-12 text-center">
@@ -40,29 +39,7 @@ const DayContainer = ({ date, forecast, maxSurf, dataKeys }: Props) => {
       </div>
       <br />
       <div className="cols-xs-12 col-md-12 text-center">
-        <div className="charts-wrapper row">
-          <div className="surf-forecast-day col-xs-12 col-md-4 text-center">
-            {/* TODO: Remove these style tags and move to Sass */}
-            <div className="surf-forecast-title" style={{ width: surfChartWidth, margin: 'auto' }}>
-              <h3>SURF</h3>
-            </div>
-            <div>
-              <svg
-                width={'100%'}
-                height={'100%'}
-                viewBox={`-100 150 ${fullWidth} ${params.height}`}
-              >
-                <StackedBarChart {...params} data={forecast} />
-              </svg>
-            </div>
-          </div>
-          <div className="tide-forecast-day col-xs-12 col-md-8 text-center">
-            {/* TODO: Remove these style tags and move to Sass */}
-            <div className="tide-forecast-title" style={{ width: tideChartWidth, margin: 'auto' }}>
-              <h3>TIDE</h3>
-            </div>
-          </div>
-        </div>
+        <GraphContainer {...params} />
       </div>
     </div>
   )
@@ -70,7 +47,7 @@ const DayContainer = ({ date, forecast, maxSurf, dataKeys }: Props) => {
 
 DayContainer.propTypes = {
   date: PropTypes.instanceOf(Array).isRequired,
-  forecast: PropTypes.instanceOf(Object).isRequired,
+  forecast: PropTypes.arrayOf(Object).isRequired,
   maxSurf: PropTypes.number.isRequired,
   dataKeys: PropTypes.instanceOf(Array).isRequired,
 }
--- src/client/ForecastPage.jsx
+++ src/client/ForecastPage.jsx
@@ -6,7 +6,10 @@ import DayContainer from './DayContainer'
 import SiteNavbar from './SiteNavbar'
 import DropdownSelector from './DropdownSelector'
 import { formatSurflineData, roundUpMaxSurfHeight } from '../shared/dataManipulation'
-import AnimatedDataWrapper from './components/stacked_bar_chart/AnimatedDataWrapper'
+import WelcomeMessage from './components/WelcomeMessage'
+import LoadingAnimation from './components/LoadingAnimation'
+import ArrowButton from './components/ArrowButton'
+import AnimatedDataWrapper from './components/animation_helpers/AnimatedDataWrapper'
 
 const regions = [
   {
@@ -102,25 +105,9 @@ class ForecastPage extends React.Component {
 
   isLoading() {
     if ((this.state.selectedSpot || this.state.selectedRegion) && !this.state.forecast) {
-      return (
-        <div className="forecast-container container-fluid">
-          <div className="text-center row">
-            <div className="wave-wrapper col-xs-12">
-              <h1 className="loading-text">LOADING...</h1>
-              <div className="wave" />
-            </div>
-          </div>
-        </div>
-      )
+      return <LoadingAnimation />
     }
-    return (
-      <div className="forecast-container container-fluid">
-        <div className="text-center row">
-          <h1 className="col-xs-12 col-md-12">WELCOME!</h1>
-          <h3 className="col-xs-12 col-md-12">Please select a location above</h3>
-        </div>
-      </div>
-    )
+    return <WelcomeMessage />
   }
 
   spotSelectorTitle() {
@@ -219,37 +206,7 @@ class ForecastPage extends React.Component {
             <div className="row is-flex">
               <div className="col-s-12 col-md-1 arrowdiv textcenter">
                 {this.state.index > 0
-                    ? <div
-                      className="previous_btn"
-                      title="Previous"
-                      onClick={() => this.arrowClick('left')}
-                      role="button"
-                    >
-                      <svg
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlnsXlink="http://www.w3.org/1999/xlink"
-                        x="0px"
-                        y="0px"
-                        width="65px"
-                        height="65px"
-                        viewBox="-11 -11.5 65 66"
-                      >
-                        <g>
-                          <g>
-                            <path
-                              fill="#474544"
-                              d="M-10.5,22.118C-10.5,4.132,4.133-10.5,22.118-10.5S54.736,4.132,54.736,22.118
-                c0,17.985-14.633,32.618-32.618,32.618S-10.5,40.103-10.5,22.118z M-8.288,22.118c0,16.766,13.639,30.406,30.406,30.406 c16.765,0,30.405-13.641,30.405-30.406c0-16.766-13.641-30.406-30.405-30.406C5.35-8.288-8.288,5.352-8.288,22.118z"
-                            />
-                            <path
-                              fill="#474544"
-                              d="M25.43,33.243L14.628,22.429c-0.433-0.432-0.433-1.132,0-1.564L25.43,10.051c0.432-0.432,1.132-0.432,1.563,0 c0.431,0.431,0.431,1.132,0,1.564L16.972,21.647l10.021,10.035c0.432,0.433,0.432,1.134,0,1.564 c-0.215,0.218-0.498,0.323-0.78,0.323C25.929,33.569,25.646,33.464,25.43,33.243z"
-                            />
-                          </g>
-                        </g>
-                      </svg>
-                    </div>
+                    ? <ArrowButton orientation={'left'} onClick={() => this.arrowClick('left')} />
                     : <div className="previous_btn" title="Previous" />}
               </div>
               <div className="col-s-12 col-md-10">
@@ -257,36 +214,7 @@ class ForecastPage extends React.Component {
               </div>
               <div className="col-s-12 col-md-1 arrowdiv textcenter">
                 {this.state.index < unformattedSurfData.dateStamp.length - 1
-                    ? <div
-                      className="next_btn"
-                      title="Next"
-                      onClick={() => this.arrowClick('right')}
-                      role="button"
-                    >
-                      <svg
-                        version="1.1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        xmlnsXlink="http://www.w3.org/1999/xlink"
-                        x="0px"
-                        y="0px"
-                        width="65px"
-                        height="65px"
-                        viewBox="-11 -11.5 65 66"
-                      >
-                        <g>
-                          <g>
-                            <path
-                              fill="#474544"
-                              d="M22.118,54.736C4.132,54.736-10.5,40.103-10.5,22.118C-10.5,4.132,4.132-10.5,22.118-10.5 c17.985,0,32.618,14.632,32.618,32.618C54.736,40.103,40.103,54.736,22.118,54.736z M22.118-8.288 c-16.765,0-30.406,13.64-30.406,30.406c0,16.766,13.641,30.406,30.406,30.406c16.768,0,30.406-13.641,30.406-30.406 C52.524,5.352,38.885-8.288,22.118-8.288z"
-                            />
-                            <path
-                              fill="#474544"
-                              d="M18.022,33.569c 0.282,0-0.566-0.105-0.781-0.323c-0.432-0.431-0.432-1.132,0-1.564l10.022-10.035 L17.241,11.615c 0.431-0.432-0.431-1.133,0-1.564c0.432-0.432,1.132-0.432,1.564,0l10.803,10.814c0.433,0.432,0.433,1.132,0,1.564 L18.805,33.243C18.59,33.464,18.306,33.569,18.022,33.569z"
-                            />
-                          </g>
-                        </g>
-                      </svg>
-                    </div>
+                    ? <ArrowButton orientation={'right'} onClick={() => this.arrowClick('right')} />
                     : <div className="next_btn" title="Next" />}
               </div>
             </div>
