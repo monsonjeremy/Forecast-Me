3095c4ffc173ff99610325662e775de3570ccffe
Jeremy Monson <monson.jeremy@gmail.com>
2017-11-12 19:49:18 -0800

    Imports update

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -1,2 +1,3 @@
 export { default as DataInterpolationWrapper } from './DataInterpolationWrapper'
 export { default as ParentResize } from './ParentResize'
+export { default as WelcomeContainer } from './WelcomeMessageContainer'
