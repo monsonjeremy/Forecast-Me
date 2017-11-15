2d13a932ab90d69f375c91e9fcf8cae016b5a769
Jeremy Monson <monson.jeremy@gmail.com>
2017-11-15 15:58:43 -0800

    Fixed an overlooked issue with rendering the buoy forecast for future days (buoys cant predict the future, man)

--- client/src/components/ForecastHeader.jsx
+++ client/src/components/ForecastHeader.jsx
@@ -11,9 +11,10 @@ type Props = {
   date: Array<string>,
   spotName: string,
   buoy: Object,
+  activeDay: number,
 }
 
-const ForecastHeader = ({ date, spotName, buoy, }: Props) => {
+const ForecastHeader = ({ date, spotName, buoy, activeDay, }: Props) => {
   const userTimezone = moment.tz.guess()
   const buoyTimeUTC = moment.utc(
     `${buoy['#YY'].data}/${buoy.MM.data}/${buoy.DD.data} ${buoy.hh.data}:${buoy.mm.data}`,
@@ -29,27 +30,31 @@ const ForecastHeader = ({ date, spotName, buoy, }: Props) => {
           <h1 className="section-title spot-name">{spotName}</h1>
           <h2 className="section-subtitle forecast-date">{date}</h2>
         </div>
-        <div className="day-data-header-container">
-          <div className="data-header-item">
-            <h3>
-              Buoy Swell Height:{' '}
-              {Math.round(10 * convertMetersToFeet(parseFloat(buoy.SwH.data))) / 10} ft
-            </h3>
+        {activeDay === 0 && (
+          <div className="day-data-header-container">
+            <div className="data-header-item">
+              <h3>
+                Buoy Swell Height:{' '}
+                {Math.round(10 * convertMetersToFeet(parseFloat(buoy.SwH.data))) / 10} ft
+              </h3>
+            </div>
+            <div className="data-header-item">
+              <h3>
+                Buoy Swell Period: {buoy.SwP.data} {buoy.SwP.units}
+              </h3>
+            </div>
+            <div className="data-header-item">
+              <h3>
+                Buoy Swell Direction: {buoy.SwD.data} - {buoy.MWD.data}&#176;
+              </h3>
+            </div>
           </div>
-          <div className="data-header-item">
-            <h3>
-              Buoy Swell Period: {buoy.SwP.data} {buoy.SwP.units}
-            </h3>
+        )}
+        {activeDay === 0 && (
+          <div className="buoy-last-updated">
+            <p>Buoy data last updated: {buoyTimeLocal}</p>
           </div>
-          <div className="data-header-item">
-            <h3>
-              Buoy Swell Direction: {buoy.SwD.data} - {buoy.MWD.data}&#176;
-            </h3>
-          </div>
-        </div>
-        <div className="buoy-last-updated">
-          <p>Buoy data last updated: {buoyTimeLocal}</p>
-        </div>
+        )}
       </div>
     </div>
   )
@@ -59,6 +64,7 @@ ForecastHeader.propTypes = {
   spotName: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   buoy: PropTypes.instanceOf(Object).isRequired,
+  activeDay: PropTypes.number.isRequired,
 }
 
 ForecastHeader.defaultProps = {}
